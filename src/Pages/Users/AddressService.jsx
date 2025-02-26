8b6cb5fd4c6218dd0576dcd2a18acc0a49060038
Amith Krishnan <amithkrishnan20@gmail.com>
2025-02-26 10:25:26 +0530

    user address

--- src/Pages/Users/AddressService.jsx
+++ src/Pages/Users/AddressService.jsx
@@ -1,12 +1,23 @@
 import { commonApi } from "../../Services/commonApi";
 import { BASE_URL } from "../../Services/base_Url";
 
+
 export const updateAddress = async (addressId, addressData) => {
   try {
     const response = await commonApi("PATCH", `${BASE_URL}/purple/addresses/${addressId}/`, addressData);
     return response.data;
   } catch (error) {
     console.error("Error updating address:", error);
+    throw error;
+  }
+};
+
+export const createAddress = async (addressData) => {
+  try {
+    const response = await commonApi("POST", `${BASE_URL}/purple/addresses/`, addressData);
+    return response.data;
+  } catch (error) {
+    console.error("Error creating address:", error);
     throw error;
   }
 };
